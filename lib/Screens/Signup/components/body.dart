7e98b47cd6e838f05b33b35b2d81eea044d63c4e
nirmalrabari <nirmalmrabari@gmail.com>
2020-07-26 15:13:53 +0530

    welcome

---
+++ lib/Screens/Signup/components/body.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/Login/login_screen.dart';
+import 'package:flutter_auth/Screens/Signup/components/background.dart';
+import 'package:flutter_auth/Screens/Homepage/homepage_screen.dart';
+import 'package:flutter_auth/components/already_have_an_account_acheck.dart';
+import 'package:flutter_auth/components/rounded_button.dart';
+import 'package:flutter_auth/components/rounded_input_field.dart';
+import 'package:flutter_auth/components/rounded_password_field.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "SIGNUP",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+            // SizedBox(height: size.height * 0.03),
+            // SvgPicture.asset(
+            //   "assets/icons/signup.svg",
+            //   height: size.height * 0.35,
+            // ),
+            SizedBox(height: size.height * 0.03),
+            RoundedInputField(
+              hintText: "Name",
+              onChanged: (value) {},
+            ),
+
+            RoundedInputField(
+              hintText: "Your Email",
+              onChanged: (value) {},
+            ),
+            RoundedPasswordField(
+              onChanged: (value) {},
+            ),
+            RoundedInputField(
+              hintText: "Phone Number",
+              onChanged: (value) {},
+            ),
+
+            RoundedButton(
+              text: "SIGNUP",
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return HomeScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+            SizedBox(height: size.height * 0.03),
+            AlreadyHaveAnAccountCheck(
+              login: false,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
